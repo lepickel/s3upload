83a2959cb3adf6da4db824d6daa9c3339213856c
lepickel <126589264+lepickel@users.noreply.github.com>
2024-09-30 17:30:20 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_NAME="s3upload.sh"
+INSTALL_DIR="/usr/local/bin"
+
+# Copy script to installation directory
+if cp "bin/$SCRIPT_NAME" "$INSTALL_DIR/s3uplaod.sh"; then
+    echo "Installation complete. You can now run 's3upload.sh' from anywhere."
+else
+    echo "Installation failed. You may need to use 'sudo' to install in $INSTALL_DIR."
+fi
+
+# Make the script executable
+chmod +x "$INSTALL_DIR/s3upload.sh"
