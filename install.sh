51017cc8d898d562e653cbafeef8c320c31fffe6
lepickel <126589264+lepickel@users.noreply.github.com>
2024-10-02 15:08:02 -0400

    fixed install.sh typo

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ SCRIPT_NAME="s3upload.sh"
 INSTALL_DIR="/usr/local/bin"
 
 # Copy script to installation directory
-if cp "bin/$SCRIPT_NAME" "$INSTALL_DIR/s3uplaod.sh"; then
+if cp "bin/$SCRIPT_NAME" "$INSTALL_DIR/s3upload.sh"; then
     echo "Installation complete. You can now run 's3upload.sh' from anywhere."
 else
     echo "Installation failed. You may need to use 'sudo' to install in $INSTALL_DIR."
